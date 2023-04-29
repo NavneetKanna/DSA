05c9bce92889f20530423a832bbb4783f0655738
NavneetKanna <navneetkanna04@gmail.com>
2023-04-29 23:14:21 +0100

    bubble

--- Bubble_Sort/main.py
+++ Bubble_Sort/main.py
@@ -1,3 +1,6 @@
+# Time Complexity = (n-1) iterations x (n-1) comparisons = O(n2)
+# For each iteration we are doing comparisons
+# Space Complexity = since no additional memory is used = O(1) 
 
 def bubble_sort(arr):
     n = len(arr)
