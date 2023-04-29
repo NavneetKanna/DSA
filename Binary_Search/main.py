f85dd5dc57e5ed42633f931af6daff6611a9a965
NavneetKanna <navneetkanna04@gmail.com>
2023-04-29 22:16:09 +0100

    starting

---
+++ Binary_Search/main.py
@@ -0,0 +1,22 @@
+def binary_search(arr, val):
+    low = 0
+    high = len(arr) - 1
+
+    while low <= high:
+        mid = (low+high) // 2
+
+        if arr[mid] == val:
+            return mid
+        elif arr[mid] < val:
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    return "Does not exist" 
+
+
+test_list = [1,3,9,11,15,19,29]
+test_val1 = 25
+test_val2 = 15
+print(binary_search(test_list, test_val1))
+print(binary_search(test_list, test_val2))
