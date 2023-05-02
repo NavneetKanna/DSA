415678c98d82093c842807f9191841501987ebee
NavneetKanna <navneetkanna04@gmail.com>
2023-05-02 16:52:24 +0100

    qucik

--- Quick_Sort/main.py
+++ Quick_Sort/main.py
@@ -1,3 +1,7 @@
+# https://www.youtube.com/watch?v=Vtckgz38QHs
+
+# Time Complexity = O(n2)
+# Space Complexity = O(logn)
 
 def quicksort(arr, l, r):
     if l < r:
@@ -6,14 +10,14 @@ def quicksort(arr, l, r):
         quicksort(arr, piviot + 1, r)
 
 def partition(arr, l , r):
-    piviot = r
+    pivot = r
     i = l - 1
 
     for j in range(l, r):
-        if arr[j] < arr[piviot]:
+        if arr[j] < arr[pivot]:
             i += 1
             arr[j], arr[i] = arr[i], arr[j]
-    arr[i+1], arr[piviot] = arr[piviot], arr[i+1]   
+    arr[i+1], arr[pivot] = arr[pivot], arr[i+1]   
 
     return i + 1
 
@@ -22,3 +26,19 @@ if __name__ == '__main__':
     quicksort(test_arr, 0, len(test_arr) - 1)
     print(test_arr)
 
+
+
+"""
+An alternatice approach, but not suitable for large arrays 
+
+def quicksort(array):
+    if len(array) <= 1:
+        return array
+
+    pivot = array[len(array) // 2]
+    left = [x for x in array if x < pivot]
+    middle = [x for x in array if x == pivot]
+    right = [x for x in array if x > pivot]
+
+    return quicksort(left) + middle + quicksort(right)
+"""
